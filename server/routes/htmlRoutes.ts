9cd40e7315cc0954d810e094882677db1fcf43f4
Nathanael Hauser <nathanaelhauser@gmail.com>
2023-05-03 17:25:03 -0700

    - Replaced default import to require in htmlRoutes.ts

--- server/routes/htmlRoutes.ts
+++ server/routes/htmlRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import path from "path";
+const path = require("path");
 
 const router = Router();
 
 // Create a catch all route that will send the user to the React app
 // This is the only route that will be used in this app
 router.get("*", (req, res) => {
-  return res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  return res.sendFile(
+    path.join(__dirname, "..", "..", "client", "build", "index.html")
+  );
 });
 
 export default router;
